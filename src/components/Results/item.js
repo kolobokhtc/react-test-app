0696111ce024edef25a826ad34f3c3b7f822bf00
Vladimir Pavlov <kolobokhtc@gmail.com>
2017-08-16 17:05:44 +0300

    setup lock when processing request

--- src/components/Results/item.js
+++ src/components/Results/item.js
@@ -8,49 +8,65 @@ import {apiUrl} from "../../config";
 
 export default class ResultItem extends Component {
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            busy: false
+        }
+    }
+
     handleApprove(event) {
+
+        event.preventDefault();
 
         var self = this;
         const {item} = this.props;
 
         item.isApproved = true;
 
+        self.setState({busy: true})
         reqwest({
             url: apiUrl + '/results/' + item.id,
             method: 'put',
             type: 'json',
-            data: item,
-            success: function (resp) {
-                if (typeof self.props.onApprove == "function") {
-                    self.props.onApprove();
-                }
+            data: item
+        }).then(function (resp) {
+            if (typeof self.props.onApprove == "function") {
+                self.props.onApprove();
             }
+        }).always(function (resp) {
+            self.setState({busy: false})
         });
 
-        event.preventDefault();
+
     }
 
     handleDelete(event) {
+
+        event.preventDefault();
 
         var self = this;
         const {item} = this.props;
 
         reqwest({
             url: apiUrl + '/results/' + item.id,
-            method: 'delete',
+            method: 'put',
             type: 'json',
-            success: function (resp) {
-                if (typeof self.props.onDelete == "function") {
-                    self.props.onDelete();
-                }
+            data: item
+        }).then(function (resp) {
+            if (typeof self.props.onDelete == "function") {
+                self.props.onDelete();
             }
+        }).always(function (resp) {
+            self.setState({busy: false})
         });
 
-        event.preventDefault();
     }
 
     render() {
         const {item} = this.props;
+        const {busy} = this.state;
         return <div>
             {item.isApproved == true ? <div>Approved</div> : null }
             <div>
@@ -63,8 +79,8 @@ export default class ResultItem extends Component {
                     <p>{item.usersText}</p>
                 </div>
             </div>
-            <button onClick={this.handleDelete.bind(this)}>Delete</button>
-            <button onClick={this.handleApprove.bind(this)}>Approve</button>
+            { busy ? <div>processing</div> : <div><button onClick={this.handleDelete.bind(this)}>Delete</button><button onClick={this.handleApprove.bind(this)}>Approve</button></div>}
+
         </div>
     }
 
